0919dc79ccc7f5d1c25de36641f21fa57eeaf04b
Chart <Chartfizz@outlook.com>
2022-12-20 16:56:38 +0700

    finish 1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
+int main(){
 
-int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+   
+    int a  =  20, b = 30;
+    int c = 4;
+    
+    float outpot;
+    
+    
+    
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    outpot = double (a) - double(b)/double(c);
+    cout << "output: " << outpot << "\n";
     return 0;
 }
+
